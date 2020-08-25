07084f906dfb49ceeeedeec28db2ad290e688df0
mikkolaj <bulmikolaj@interia.pl>
2020-08-25 14:31:52 +0200

    cośtam

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,13 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
-dotenv.config({ path: './config.env '});
+dotenv.config({ path: './config.env ' });
 const app = require('./app');
+
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD',
+    process.env.DATABASE_PASSWORD
+);
 
 const port = 8000;
 const server = app.listen(port, () => {
