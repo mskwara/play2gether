61f364103770038fdfc07d91b3491c08497c5b28
Michał Skwara <michalon2000@gmail.com>
2020-08-26 14:01:36 +0200

    GameTile

---
+++ frontend/src/views/Home.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import GameTile from "../components/GameTile";
+import "./Home.scss";
+import axios from "axios";
+
+const Home = (props) => {
+    const [state, setState] = useState({
+        games: null,
+        loading: true,
+    });
+    useEffect(() => {
+        const getData = async () => {
+            try {
+                const res = await axios.get("http://localhost:8000/games");
+                setState((state) => ({
+                    ...state,
+                    games: res.data.data.data,
+                    loading: false,
+                }));
+            } catch (err) {
+                console.log(err);
+            }
+        };
+        getData();
+    }, []);
+
+    // transform games data to gameTile components
+    let gameTiles = null;
+    if (state.games !== null) {
+        gameTiles = state.games.map((game) => (
+            <GameTile game={game} key={game._id} />
+        ));
+    }
+
+    return <div id="Home">{gameTiles}</div>;
+};
+
+export default Home;
