49af26cf4e292febf0c094eb9a3ce559f51a51c7
Michał Skwara <michalon2000@gmail.com>
2020-08-26 21:55:59 +0200

    Game, loader

--- frontend/src/components/GameTile/GameTile.js
+++ frontend/src/components/GameTile/GameTile.js
@@ -7,8 +7,15 @@ const GameTile = (props) => {
     const tileStyle = {
         backgroundImage: `url(${bgImage})`,
     };
+
+    const tileClickHandler = () => {
+        props.history.push({
+            pathname: `/games/${props.game._id}`,
+        });
+    };
+
     return (
-        <div id="GameTile" style={tileStyle}>
+        <div id="GameTile" style={tileStyle} onClick={tileClickHandler}>
             <h1>{props.game.title}</h1>
             <div className="description">
                 <p>{props.game.description}</p>
