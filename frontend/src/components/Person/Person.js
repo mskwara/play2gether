9609dd6dd5fb4bc9bac6b6cf4fd09533673d1cc3
Michał Skwara <michalon2000@gmail.com>
2020-08-27 14:27:01 +0200

    person tile, interactive topbar

--- frontend/src/components/Person/Person.js
+++ frontend/src/components/Person/Person.js
@@ -4,7 +4,29 @@ import "./Person.scss";
 const Person = (props) => {
     return (
         <div id="Person" className={props.className}>
-            Tu będzie jakiś player
+            <div className="slider">
+                <div className="user">
+                    <p>Vexorth</p>
+                </div>
+                <img src={require("../../assets/default.png")} alt="photo" />
+                <div className="buttons">
+                    <img
+                        src={require("../../assets/invite_to_game.png")}
+                        alt="photo"
+                        className="btn"
+                    />
+                    <img
+                        src={require("../../assets/message.png")}
+                        alt="photo"
+                        className="btn"
+                    />
+                    <img
+                        src={require("../../assets/add_friend.png")}
+                        alt="photo"
+                        className="btn"
+                    />
+                </div>
+            </div>
         </div>
     );
 };
