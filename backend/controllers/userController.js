d63d38d15d703c84f05805d852c3ccfe0770434b
unknown <bulmikolaj@interia.pl>
2020-08-27 13:48:32 +0200

    logged in endpoint

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,12 @@
 const User = require('./../models/userModel');
 const factory = require('./handlerFactory');
+const catchAsync = require('./../utils/catchAsync');
 
+exports.getAllUsers = factory.getAll(User, '');
 
-exports.getAllUsers = factory.getAll(User);
+exports.isLoggedIn = catchAsync(async (req, res, next) => {
+    res.status(200).json({
+        status: 'success',
+        user: req.user
+    });
+});
