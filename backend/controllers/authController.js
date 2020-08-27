ce4842fed3ce598a70ff1d643be22d423dbf4bf2
Michał Skwara <michalon2000@gmail.com>
2020-08-27 19:00:11 +0200

    login, cookie

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,12 +1,12 @@
-const jwt = require('jsonwebtoken');
-const { promisify } = require('util');
-const User = require('./../models/userModel');
-const catchAsync = require('./../utils/catchAsync');
-const AppError = require('../utils/appError');
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+const User = require("./../models/userModel");
+const catchAsync = require("./../utils/catchAsync");
+const AppError = require("../utils/appError");
 
 function authToken(id) {
     return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
+        expiresIn: process.env.JWT_EXPIRES_IN,
     });
 }
 
@@ -14,23 +14,24 @@ function createAuthToken(user, statusCode, res) {
     const token = authToken(user.id);
 
     const cookieOptions = {
-        expires: new Date(Date.now() + process.env.JWT_EXPIRES_IN * 1000 * 60 * 60 * 24),
-        httpOnly: true
+        expires: new Date(
+            Date.now() + process.env.JWT_EXPIRES_IN * 1000 * 60 * 60 * 24
+        ),
+        httpOnly: true,
     };
 
-    if (process.env.NODE_ENV === 'production')
-        cookieOptions.secure = true;
+    if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
 
     user.password = undefined;
 
-    res.cookie('jwt', token, cookieOptions);
+    res.cookie("jwt", token, cookieOptions);
 
     res.status(statusCode).json({
-        status: 'success',
+        status: "success",
         token,
         data: {
-            user
-        }
+            user,
+        },
     });
 }
 
@@ -39,7 +40,7 @@ exports.signup = catchAsync(async (req, res, next) => {
         name: req.body.name,
         email: req.body.email,
         password: req.body.password,
-        passwordConfirm: req.body.passwordConfirm
+        passwordConfirm: req.body.passwordConfirm,
     });
 
     createAuthToken(newUser, 201, res);
@@ -50,14 +51,14 @@ exports.login = catchAsync(async (req, res, next) => {
 
     // 1) Check if email and password exist
     if (!email || !password) {
-        return next(new AppError('Please provide email and password!', 400));
+        return next(new AppError("Please provide email and password!", 400));
     }
 
     // 2) Check if user exists and password is correct
-    const user = await User.findOne({ email }).select('+password');
+    const user = await User.findOne({ email }).select("+password");
 
     if (!user || !(await user.correctPassword(password, user.password))) {
-        return next(new AppError('Incorrect email or password', 401));
+        return next(new AppError("Incorrect email or password", 401));
     }
 
     // 3) If everything ok, send token to client
@@ -67,12 +68,17 @@ exports.login = catchAsync(async (req, res, next) => {
 exports.protect = catchAsync(async (req, res, next) => {
     // 1) Getting token
     let token;
-    if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
+    if (
+        req.headers.authorization &&
+        req.headers.authorization.startsWith("Bearer")
+    ) {
+        token = req.headers.authorization.split(" ")[1];
     }
 
     if (!token) {
-        return next(new AppError('You are not logged in. Log in to get access', 401));
+        return next(
+            new AppError("You are not logged in. Log in to get access", 401)
+        );
     }
 
     // 2) Verification of the token
@@ -80,13 +86,18 @@ exports.protect = catchAsync(async (req, res, next) => {
 
     // 3) Check if user still exists
     const currentUser = await User.findById(decoded.id);
-    if(!currentUser) {
-        return next(new AppError('The user does not longer exist', 401));
+    if (!currentUser) {
+        return next(new AppError("The user does not longer exist", 401));
     }
 
     // 4) Check if user changed password after token was issued
     if (currentUser.changedPasswordAfter(decoded.iat)) {
-        return next(new AppError('User recently changed password! Please log in again!', 401));
+        return next(
+            new AppError(
+                "User recently changed password! Please log in again!",
+                401
+            )
+        );
     }
 
     // Grant access to protected route
