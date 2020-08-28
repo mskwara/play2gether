203b66f58b3103b3cfd54697b2fd5dc906587070
Michał Skwara <michalon2000@gmail.com>
2020-08-28 11:23:10 +0200

    cookie working (probably)

--- frontend/src/components/Topbar/Topbar.js
+++ frontend/src/components/Topbar/Topbar.js
@@ -3,33 +3,53 @@ import { Link } from "react-router-dom";
 import "./Topbar.scss";
 
 const Topbar = (props) => {
+    let links = null;
+    console.log(localStorage.getItem("userId"));
+    // if (
+    //     localStorage.getItem("userId") !== null &&
+    //     localStorage.getItem("userId") !== undefined
+    // ) {
+    //     links = [
+    //         <div className="link" key="1">
+    //             <Link to="/">Home</Link>
+    //             <div className="underline" />
+    //         </div>,
+    //         <div className="link" key="2">
+    //             <Link>Friends</Link>
+    //             <div className="underline" />
+    //         </div>,
+    //         <div className="link" key="3">
+    //             <Link>Favourites</Link>
+    //             <div className="underline" />
+    //         </div>,
+    //         <div className="link" key="4">
+    //             <Link>Logout</Link>
+    //             <div className="underline" />
+    //         </div>,
+    //     ];
+    // } else {
+    links = [
+        <div className="link" key="1">
+            <Link to="/">Home</Link>
+            <div className="underline" />
+        </div>,
+        <div className="link" key="2">
+            <Link onClick={props.openSignup}>Sign up</Link>
+            <div className="underline" />
+        </div>,
+        <div className="link" key="3">
+            <Link onClick={props.openLogin}>Login</Link>
+            <div className="underline" />
+        </div>,
+    ];
+    // }
+
     return (
         <div id="Topbar" className="normal">
             <Link className="title" to="/">
                 Play2gether
             </Link>
-            <div className="links">
-                <div className="link">
-                    <Link to="/">Home</Link>
-                    <div className="underline" />
-                </div>
-                <div className="link">
-                    <Link>Friends</Link>
-                    <div className="underline" />
-                </div>
-                <div className="link">
-                    <Link>Favourites</Link>
-                    <div className="underline" />
-                </div>
-                <div className="link">
-                    <Link onClick={props.openSignup}>Sign up</Link>
-                    <div className="underline" />
-                </div>
-                <div className="link">
-                    <Link onClick={props.openLogin}>Login</Link>
-                    <div className="underline" />
-                </div>
-            </div>
+            <div className="links">{links}</div>
         </div>
     );
 };
