de7ea36579ca6da8cf323dbd480a8b8ae43ce249
Michał Skwara <michalon2000@gmail.com>
2020-09-03 16:00:37 +0200

    Profile(not finished), visual changes

--- frontend/src/components/Person/Person.js
+++ frontend/src/components/Person/Person.js
@@ -2,9 +2,16 @@ import React, { useContext } from "react";
 import "./Person.scss";
 import request from "../../utils/request";
 import UserContext from "../../utils/UserContext";
+import ConvContext from "../../utils/ConvContext";
+import SocketContext from "../../utils/SocketContext";
+import PopupContext from "../../utils/PopupContext";
 
 const Person = (props) => {
     const userContext = useContext(UserContext);
+    const convContext = useContext(ConvContext);
+    const socketContext = useContext(SocketContext);
+    const popupContext = useContext(PopupContext);
+    const conversations = userContext.globalUserState.conversations;
     const addFriend = async () => {
         await request(
             "patch",
@@ -13,6 +20,45 @@ const Person = (props) => {
             true
         );
     };
+
+    const openChat = () => {
+        if (
+            !userContext.globalUserState.user.friends.some(
+                (f) => f._id === props.user._id
+            )
+        ) {
+            console.log("Not implemented!");
+            return null;
+        }
+        const conv = conversations.filter(
+            (el) =>
+                el.group === false &&
+                el.participants.some((p) => p._id === props.user._id)
+        )[0];
+        const openedConvs = [...convContext.convState.openedConvs];
+        if (openedConvs.filter((c) => c._id === conv._id).length > 0) {
+            //this conv has been opened already
+            return;
+        } else {
+            openedConvs.push(conv);
+        }
+        convContext.updateConvState({ openedConvs });
+        return conv;
+    };
+
+    const inviteToGame = () => {
+        const conv = openChat();
+        if (!conv) {
+            console.log("Not implemented!");
+            return;
+        }
+        socketContext.socketState.socket.emit("send", {
+            message: `Hi! Would you like to play ${props.gameTitle} with me?`,
+            jwt: userContext.globalUserState.jwt,
+            room: conv._id,
+        });
+    };
+
     let addFriendButton = null;
 
     if (
@@ -47,11 +93,13 @@ const Person = (props) => {
                     src={require("../../assets/invite_to_game.png")}
                     alt="button"
                     className="btn"
+                    onClick={inviteToGame}
                 />
                 <img
                     src={require("../../assets/message.png")}
                     alt="button"
                     className="btn"
+                    onClick={openChat}
                 />
                 {addFriendButton}
             </div>
@@ -68,6 +116,9 @@ const Person = (props) => {
                 <img
                     src={require(`../../../../backend/static/users/${props.user.photo}`)}
                     alt="avatar"
+                    onClick={() =>
+                        popupContext.setProfileOpened(true, props.user._id)
+                    }
                 />
                 {buttons}
             </div>
