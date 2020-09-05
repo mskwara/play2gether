12ee19648c28289a94f01fe6f4859f3039e2b58c
Michał Skwara <michalon2000@gmail.com>
2020-09-05 23:05:36 +0200

    update user, code optimization

--- frontend/src/components/DialogWindow/Signup/Signup.js
+++ frontend/src/components/DialogWindow/Signup/Signup.js
@@ -1,9 +1,9 @@
 import React, { useState, useContext } from "react";
-import MyInput from "../MyInput/MyInput";
+import MyInput from "../../MyInput/MyInput";
 import "./Signup.scss";
-import request from "../../utils/request";
-import PopupContext from "../../utils/PopupContext";
-import UserContext from "../../utils/UserContext";
+import request from "../../../utils/request";
+import PopupContext from "../../../utils/PopupContext";
+import UserContext from "../../../utils/UserContext";
 
 const Signup = (props) => {
     const popupContext = useContext(PopupContext);
@@ -44,7 +44,7 @@ const Signup = (props) => {
                 user: res.data.data,
                 conversations: convRes.data.data,
             });
-            props.closeSignup();
+            popupContext.closeDialogWindow();
             popupContext.setAlertActive(
                 true,
                 `Welcome to our community, ${res.data.data.name}!`
@@ -67,48 +67,48 @@ const Signup = (props) => {
 
     return (
         <div id="Signup" className={props.className}>
-            <div className="form">
-                <h1>Sign up</h1>
-                <img
-                    src={require("../../assets/close.png")}
-                    alt="close"
-                    className="close-btn"
-                    onClick={props.closeSignup}
-                />
-                <MyInput
-                    type="text"
-                    name="name"
-                    placeholder="Name"
-                    value={userState.name}
-                    labelId="signup"
-                    handleInputChange={handleInputChange}
-                />
-                <MyInput
-                    type="text"
-                    name="email"
-                    placeholder="Email"
-                    value={userState.email}
-                    labelId="signup"
-                    handleInputChange={handleInputChange}
-                />
-                <MyInput
-                    type="password"
-                    name="password"
-                    placeholder="Password"
-                    value={userState.password}
-                    labelId="signup"
-                    handleInputChange={handleInputChange}
-                />
-                <MyInput
-                    type="password"
-                    name="passwordConfirm"
-                    placeholder="Confirm the password"
-                    value={userState.passwordConfirm}
-                    labelId="signup"
-                    handleInputChange={handleInputChange}
-                />
-                <button onClick={signup}>Join the community</button>
-            </div>
+            <h1>Sign up</h1>
+            <img
+                src={require("../../../assets/close.png")}
+                alt="close"
+                className="close-btn"
+                onClick={popupContext.closeDialogWindow}
+            />
+            <MyInput
+                type="text"
+                name="name"
+                placeholder="Name"
+                value={userState.name}
+                labelId="signup"
+                handleInputChange={handleInputChange}
+            />
+            <MyInput
+                type="text"
+                name="email"
+                placeholder="Email"
+                value={userState.email}
+                labelId="signup"
+                handleInputChange={handleInputChange}
+            />
+            <MyInput
+                type="password"
+                name="password"
+                placeholder="Password"
+                value={userState.password}
+                labelId="signup"
+                handleInputChange={handleInputChange}
+            />
+            <MyInput
+                type="password"
+                name="passwordConfirm"
+                placeholder="Confirm the password"
+                value={userState.passwordConfirm}
+                labelId="signup"
+                handleInputChange={handleInputChange}
+            />
+            <button onClick={signup} className="button">
+                Join the community
+            </button>
         </div>
     );
 };
