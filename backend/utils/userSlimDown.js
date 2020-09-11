7ad1a24cc19b71fbd065bc54a2c00d91341fc990
unknown <bulmikolaj@interia.pl>
2020-09-11 14:00:45 +0200

    users sorted by recent activity

--- backend/utils/userSlimDown.js
+++ backend/utils/userSlimDown.js
@@ -2,5 +2,6 @@ module.exports = user => {
     user.passwordChangedAt = undefined;
     user.privateConversations = undefined;
     user.groupConversations = undefined;
+    user.recentActivity = undefined;
     return user;
 }
