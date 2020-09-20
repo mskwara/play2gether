d9e4100b2a3ff07f68336b18e6273c61ae022a62
unknown <bulmikolaj@interia.pl>
2020-09-20 16:41:44 +0200

    maybe fixed user

--- backend/controllers/commentController.js
+++ backend/controllers/commentController.js
@@ -6,12 +6,12 @@ const AppError = require('../utils/appError');
 exports.getAllComments = factory.getAll(Comment, '', '-sentAt');
 
 exports.postComment = catchAsync(async (req, res, next) => {
-    if (req.user.id === req.params.userId)
+    if (req.user._id.toString() === req.params.userId)
         return next(new AppError('You can\'t comment about yourself.', 400));
 
     await Comment.create({
         user: req.params.userId,
-        from: req.user.id,
+        from: req.user._id.toString(),
         sentAt: Date.now(),
         comment: req.body.comment
     });
@@ -25,7 +25,7 @@ exports.deleteComment = catchAsync(async (req, res, next) => {
     if (!comment)
         return next(new AppError('No comment found with that ID.', 404));
 
-    if (comment.user.toString() === req.user.id || comment.from.toString() === req.user.id) {
+    if (comment.user.toString() === req.user._id.toString() || comment.from.toString() === req.user._id.toString()) {
         await Comment.findByIdAndDelete(comment._id);
     } else {
         return next(new AppError('You have no rights to delete that comment.', 403));
