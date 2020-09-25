6173479f45426e858363c5ad528b2fe53e9fc611
mikkolaj <bulmikolaj@interia.pl>
2020-09-25 18:50:57 +0200

    hope for the best

--- backend/app.js
+++ backend/app.js
@@ -23,25 +23,25 @@ const app = express();
 app.use(cookieParser());
 
 // Serve static files
-app.use(express.static(path.join(__dirname, 'static')));
+// app.use(express.static(path.join(__dirname, 'static')));
 
 app.use(express.json({ limit: '10kb' }));
 app.use(express.urlencoded({ extended: true, limit: '10kb' }));
 
 // Testowy pajac
-app.get('/', (req, res, next) => {
-    // const cookieOptions = {
-    //     expires: new Date(Date.now() + 60 * 1000 * 60 * 60 * 24),
-    //     httpOnly: true,
-    // };
+// app.get('/', (req, res, next) => {
+//     // const cookieOptions = {
+//     //     expires: new Date(Date.now() + 60 * 1000 * 60 * 60 * 24),
+//     //     httpOnly: true,
+//     // };
 
-    // res.cookie('debil', 'rafal', cookieOptions);
+//     // res.cookie('debil', 'rafal', cookieOptions);
 
-    res.status(200).json({
-        status: 'success',
-        message: 'rafal to pajac',
-    });
-});
+//     res.status(200).json({
+//         status: 'success',
+//         message: 'rafal to pajac',
+//     });
+// });
 
 app.use('/users', userRouter);
 app.use('/games', gameRouter);
