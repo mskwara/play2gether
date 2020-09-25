d62b7e2fffa6be32fe3da30a933c92c5b9c8b0f6
mikkolaj <bulmikolaj@interia.pl>
2020-09-25 18:09:35 +0200

    changed socket connection

--- backend/static/chat.js
+++ backend/static/chat.js
@@ -1,4 +1,4 @@
-var socket = io.connect('http://localhost:8000');
+var socket = io.connect('/');
 // Query DOM
 const jwtOBJ = document.getElementById('jwt'),
     conversation = document.getElementById('conversation'),
