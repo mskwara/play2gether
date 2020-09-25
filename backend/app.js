000638fe9aa8c5f8cd8d638f993f8a745b1c63ff
mikkolaj <bulmikolaj@interia.pl>
2020-09-25 21:22:49 +0200

    cors

--- backend/app.js
+++ backend/app.js
@@ -12,14 +12,14 @@ const cookieParser = require('cookie-parser');
 
 const app = express();
 
-// app.use(cors());
-// app.options('*', cors());
-// app.use(
-//     cors({
-//         origin: 'http://localhost:3000',
-//         credentials: true,
-//     })
-// );
+if (process.env.HEROKU !== 'true') {
+    app.use(
+        cors({
+            origin: 'http://localhost:3000',
+            credentials: true,
+        })
+    );
+}
 
 app.use(cookieParser());
 
