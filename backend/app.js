b179337b346cfdc740bbba4a7380b1aa36eedb8f
mikkolaj <bulmikolaj@interia.pl>
2020-09-25 16:18:16 +0200

    rak again

--- backend/app.js
+++ backend/app.js
@@ -14,12 +14,12 @@ const app = express();
 
 // app.use(cors());
 // app.options('*', cors());
-app.use(
-    cors({
-        origin: 'http://localhost:3000',
-        credentials: true,
-    })
-);
+// app.use(
+//     cors({
+//         origin: 'http://localhost:3000',
+//         credentials: true,
+//     })
+// );
 app.use(cookieParser());
 
 // Serve static files
