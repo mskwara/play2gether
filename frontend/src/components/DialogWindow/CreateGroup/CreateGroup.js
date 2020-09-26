3543ca50df6e2c479a631323902afd38be17e1f6
Michał Skwara <michalon2000@gmail.com>
2020-09-26 12:22:59 +0200

    more intervals, bug fixes

--- frontend/src/components/DialogWindow/CreateGroup/CreateGroup.js
+++ frontend/src/components/DialogWindow/CreateGroup/CreateGroup.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState, useEffect } from "react";
+import React, { useContext, useState } from "react";
 import "./CreateGroup.scss";
 import UserContext from "../../../utils/UserContext";
 import PopupContext from "../../../utils/PopupContext";
